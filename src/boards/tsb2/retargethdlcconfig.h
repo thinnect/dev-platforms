fef6ca1702884ba98bfdbf551dae947365f7a19b
Raido Pahtma <raidopahtma@gmail.com>
2020-01-08 13:29:44 +0200

    Update ext uart hdlc config.

--- src/boards/tsb2/retargethdlcconfig.h
+++ src/boards/tsb2/retargethdlcconfig.h
@@ -14,17 +14,17 @@
 #define SERIAL_HDLC_CLOCK       cmuClock_USART1
 #define SERIAL_HDLC_IRQn        USART1_RX_IRQn
 #define SERIAL_HDLC_UART_INDEX  1
-#define SERIAL_HDLC_TXPORT         gpioPortA
+#define SERIAL_HDLC_TXPORT      gpioPortA
 #define SERIAL_HDLC_TXPIN       2
 #define SERIAL_HDLC_RXPORT      gpioPortA
 #define SERIAL_HDLC_RXPIN       1
 
 #else // Use the non-JTAG uart
 
-#define SERIAL_HDLC_UART     USART2
-#define SERIAL_HDLC_IRQ_NAME USART2_RX_IRQHandler
-#define SERIAL_HDLC_CLOCK    cmuClock_USART2
-#define SERIAL_HDLC_IRQn     USART2_RX_IRQn
+#define SERIAL_HDLC_UART        USART2
+#define SERIAL_HDLC_IRQ_NAME    USART2_RX_IRQHandler
+#define SERIAL_HDLC_CLOCK       cmuClock_USART2
+#define SERIAL_HDLC_IRQn        USART2_RX_IRQn
 #define SERIAL_HDLC_UART_INDEX  2
 #define SERIAL_HDLC_TXPORT      gpioPortC
 #define SERIAL_HDLC_TXPIN       1

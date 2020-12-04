35a8e09f1841474c7ebb8a78a0a8e31496bcfe57
German <german.mundinger@gmail.com>
2020-12-04 15:42:04 +0200

    Fix tsb0, tsb2

--- src/boards/tsb0/platform_io.h
+++ src/boards/tsb0/platform_io.h
@@ -32,6 +32,9 @@
 #define PLATFORM_BUTTON_PORT gpioPortF
 #define PLATFORM_BUTTON_PIN  4
 
+#define PLATFORM_I2C_COUNT 1
+#define PLATFORM_I2CS { I2C0 }
+
 #define PLATFORM_ADC_COUNT 1
 #define PLATFORM_ADCS { ADC0 }
 
